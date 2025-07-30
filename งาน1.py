fc7e08ebdec877e2f03f6d2dff9a55e235f2e52c
Siraphat36 <nikkichk159@gmail.com>
2025-07-30 12:58:13 +0700

    Update งาน1.py

---
+++ งาน1.py
@@ -0,0 +1,7 @@
+a = int(input("ใส่เลขที่ 1 :"))
+b = int(input("ใส่เลขที่ 2 :"))
+c = int(input("ใส่เลขที่ 3 :"))
+d = int(input("ใส่เลขที่ 4 :"))
+f = int(input("ใส่เลขที่ 5 :"))
+po = [a,b,c,d,f]
+print(sorted(po))
