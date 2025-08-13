e30d11b67149bdc70109c0553b4e5511f9bafd87
natthapong-jan <natthapong-jan@rcc.ac.th>
2025-08-13 13:47:22 +0700

    แก้ไข code error

--- natthapong31.py
+++ natthapong31.py
@@ -1,27 +1,13 @@
 import random
+number = random.randint(1, 100)
+print("ทายเลขระหว่าง 1 ถึง 100")
 
-def guess_the_number():
-    print("ยินดีต้านรับ")
-    print("ให้เดาเลขระหว่าง 1 - 100")
-
-    # คอมพิวเตอร์สุ่มเลข
-    number_to_guess = random.randint(1, 100)
-    attempts = 0
-
-    while True:
-        # รับค่าจากผู้เล่น
-        try:
-            guess = int(input("เดาเลย: "))
-            attempts += 1
-
-            # ตรวจสอบคำทาย
-            if guess < number_to_guess:
-                print("น้อยเกินไป.")
-            elif guess > number_to_guess:
-                print("สูงเกินไป.")
-            else:
-                print(f"ยินดีด้วยคุณทายถูกแล้ว {number_to_guess} in {attempts} attempts.")
-                break
-
-# เรียกใช้ฟังก์ชัน
-guess_the_number()
+while True:
+    guess = int(input("ใส่เลขที่คุณทาย: "))
+    if guess < number:
+        print("น้อยเกินไป")
+    elif guess > number:
+        print("มากเกินไป")
+    else:
+        print("ถูกต้อง! เลขคือ", number)
+        break
