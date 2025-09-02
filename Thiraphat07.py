3c3cf8335c0a5383f8678961d9e41c107888009a
Siraphat36 <nikkichk159@gmail.com>
2025-09-02 21:43:45 +0700

    add branch

--- Thiraphat07.py
+++
@@ -1,4 +0,0 @@
-name = input("ชื่อ: ")
-year = int(input("ค.ศ.เกิด: "))
-age = 2025 - year
-print(f"name {age} ปี")
--- ratthapoom23.py
+++
@@ -1,7 +0,0 @@
-from datetime import datetime
-N = input("ชื่อ ")
-B = input("ใส่วันเกิด (ใส่แบบ DD/MM/YYYY) ")
-S = datetime.strptime(B, "%d/%m/%Y")
-O = datetime.today()
-L = O.year - S.year
-print("ชื่อ ", N , "อายุ", L , "ปี")
